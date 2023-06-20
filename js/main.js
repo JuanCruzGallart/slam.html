8360c515cc916218f0fce52219282fc475818e15
JuanCruzGallart <gallartjuancruz@gmail.com>
2023-06-20 01:19:11 -0300

    muajaja

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 let wrapper = document.querySelector(".wrapper")
+let portada = document.querySelector("#portada")
+let nav = document.querySelector("#nav")
 let btnDigital = document.querySelector("#btn_digital")
 let btnBook = document.querySelector("#btn_book")
-let row = document.createElement("div")
-let col = document.createElement("div")
-
 
 
