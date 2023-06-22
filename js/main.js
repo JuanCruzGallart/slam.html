9c7e8cab57f130b58ac46872d9a1fcdb73235741
JuanCruzGallart <gallartjuancruz@gmail.com>
2023-06-22 00:54:29 -0300

    mirajoaco

--- js/main.js
+++ js/main.js
@@ -3,5 +3,28 @@ let portada = document.querySelector("#portada")
 let nav = document.querySelector("#nav")
 let btnDigital = document.querySelector("#btn_digital")
 let btnBook = document.querySelector("#btn_book")
+let containerImg = document.querySelector(".container-img")
+let imgs = document.querySelectorAll(".works_imgs")
+let imgContainer = document.querySelector(".img-show")
+let svg = document.querySelector(".bi", "bi-x")
+
+imgs.forEach(img => {
+    img.addEventListener("click", () => {
+        containerImg.classList.remove("d-none")
+
+        imgContainer.src = img.getAttribute("src")
 
 
+        if(!containerImg.addEventListener("click", () => {
+            containerImg.classList.add("d-none")
+
+        }))
+        
+        svg.addEventListener("click", () => {
+            containerImg.classList.add("d-none")
+        })
+    })
+})
+
+function addImage (src) {
+}
